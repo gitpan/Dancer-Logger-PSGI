6bb13b8af9e9e728ead8df9016775cb32601b321
Richard Simões <rsimoes@cpan.org>
2011-12-24 06:28:59 +0000

    Import of RSIMOES/Dancer-Logger-PSGI-0.04 from CPAN.
    gitpan-cpan-distribution: Dancer-Logger-PSGI
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         RSIMOES/Dancer-Logger-PSGI-0.04.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

--- lib/Dancer/Logger/PSGI.pm
+++ lib/Dancer/Logger/PSGI.pm
@@ -5,7 +5,7 @@ use warnings;
 use Dancer::SharedData;
 use base "Dancer::Logger::Abstract";
 
-our $VERSION = '0.020_001'; # VERSION
+our $VERSION = '0.04'; # VERSION
 # ABSTRACT: PSGI Log handler for Dancer
 
 sub init {}
@@ -38,32 +38,37 @@ Dancer::Logger::PSGI - PSGI Log handler for Dancer
 
 =head1 VERSION
 
-version 0.020_001
+version 0.04
 
 =head1 SYNOPSIS
 
-In your Dancer's configuration file:
+In your Dancer's environment file:
 
     logger: PSGI
+    - plack_middlewares:
+      -
+        - ConsoleLogger
 
 In your application
 
     warning "this is a warning"
-
-Then, in your app.psgi
-
-    $app = builder { enable "ConsoleLogger"; $app; }
-
-or in your environment file:
-
-    - plack_middlewares:
-      - ConsoleLogger
 
 With L<Plack::Middleware::ConsoleLogger>, all your log will be send to the JavaScript console of your browser.
 
 =head1 DESCRIPTION
 
 This class is an interface between your Dancer's application and B<psgix.logger>. Message will be logged in whatever logger you decided to use in your L<Plack> handler. If no logger is defined, nothing will be logged.
+
+=head1 AUTHOR
+
+franck cuny <franck@lumberjaph.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by franck cuny.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =head1 AUTHOR
 
