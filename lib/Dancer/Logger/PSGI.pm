af1ed361df4dcdd21571607ac3883c3fdf2faac6
Richard Simões <rsimoes@cpan.org>
2014-03-13 20:59:20 +0000

    Import of RSIMOES/Dancer-Logger-PSGI-v1.0.0 from CPAN.
    gitpan-cpan-distribution: Dancer-Logger-PSGI
    gitpan-cpan-version:      v1.0.0
    gitpan-cpan-path:         RSIMOES/Dancer-Logger-PSGI-v1.0.0.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

--- lib/Dancer/Logger/PSGI.pm
+++ lib/Dancer/Logger/PSGI.pm
@@ -1,14 +1,15 @@
 package Dancer::Logger::PSGI;
 
+use v5.10;
 use strict;
 use warnings;
 use Dancer::SharedData;
-use base "Dancer::Logger::Abstract";
+use parent qw(Dancer::Logger::Abstract);
 
-our $VERSION = '0.04'; # VERSION
+our $VERSION = 'v1.0.0'; # VERSION
 # ABSTRACT: PSGI Log handler for Dancer
 
-sub init {}
+sub init { }
 
 sub _log {
     my ( $self, $level, $message ) = @_;
@@ -27,18 +28,6 @@ sub _log {
 }
 
 1;
-
-
-__END__
-=pod
-
-=head1 NAME
-
-Dancer::Logger::PSGI - PSGI Log handler for Dancer
-
-=head1 VERSION
-
-version 0.04
 
 =head1 SYNOPSIS
 
@@ -70,16 +59,4 @@ This software is copyright (c) 2010 by franck cuny.
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
-=head1 AUTHOR
-
-franck cuny <franck@lumberjaph.net>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2011 by franck cuny.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
 =cut
-
